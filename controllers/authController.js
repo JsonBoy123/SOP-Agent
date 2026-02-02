bd3613f851ca1caf6155a6512f28f0f7d7c55472
unknown <srujanavelina@email.com>
2026-02-02 20:53:12 +0000

    Initial commit - login backend with auth APIs

---
+++ controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require("../models/user");
+
+// REGISTER
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// LOGIN (simple version)
+const loginUser = async (req, res) => {
+  res.json({ message: "Login API working" });
+};
+
+module.exports = { registerUser, loginUser };
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+// ROUTE IMPORT
+app.use("/api/auth", require("./routes/authRoutes"));
+
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server started"));
